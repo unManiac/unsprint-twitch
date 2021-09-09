c7f00455a3afe9233c53c083e3507a971eb7833a
unManiac <unManiac>
2021-09-08 23:17:34 -0300

    arredondar pra cima pontos com virgula

--- src/helper.js
+++ src/helper.js
@@ -1,7 +1,7 @@
 export function calculatePoints(started, ended = Date.now(), multiplier = 1) {
   const seconds = Math.abs(ended - started) / 1000;
   const minutes = Math.ceil(seconds / 60);
-  const points = minutes * multiplier;
+  const points = Math.ceil(minutes * multiplier);
 
   return [points, minutes];
 }
